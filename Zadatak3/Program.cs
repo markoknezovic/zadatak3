a1e8d1275e91080d68459d8742eed48f4dbd6fe1
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-06 17:44:20 +0200

    Add files via upload

---
+++ Zadatak3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uneseniBroj;
+
+            Console.WriteLine(" Unesi broj: ");
+
+
+            uneseniBroj = Convert.ToInt32(Console.ReadLine());
+
+            if (uneseniBroj % 4 == 0 && uneseniBroj % 6 != 0)
+            {
+
+                Console.WriteLine(" Broj je djeljiv sa 4. ");
+
+            }
+            if (uneseniBroj % 4 != 0 && uneseniBroj % 6 == 0)
+
+            {
+                Console.WriteLine(" Broj je djeljiv sa 6. ");
+
+            }
+
+            if (uneseniBroj % 4 == 0 && uneseniBroj % 6 == 0)
+
+            {
+
+                Console.WriteLine(" Broj je djeljiv sa 4 i sa 6. ");
+
+            }
+
+            if (uneseniBroj % 4 != 0 && uneseniBroj % 6 != 0)
+
+            {
+
+                Console.WriteLine(" Broj nije djeljiv sa 4 i sa 6. ");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
